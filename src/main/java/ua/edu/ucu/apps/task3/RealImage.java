72e4c22950d9dcae04a92cbab94badc63994e6ad
LidaSemsichko <semsichko.pn@ucu.edu.ua>
2024-11-25 23:03:31 +0200

    from lab

---
+++ src/main/java/ua/edu/ucu/apps/task3/RealImage.java
@@ -0,0 +1,41 @@
+package ua.edu.ucu.apps.task3;
+
+
+import lombok.Getter;
+
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.*;
+
+@Getter
+public class RealImage implements MyImage {
+    private JFrame frame;
+    private final String filename;
+
+    public RealImage(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void display() {
+        if (GraphicsEnvironment.isHeadless()) {
+            System.out.println("Loading " + filename + " (headless mode)");
+            return;
+        }
+
+        if (frame == null) {
+            frame = new JFrame();
+            ImageIcon icon = new ImageIcon(filename);
+            JLabel label = new JLabel(icon);
+            frame.add(label);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.pack();
+        }
+        frame.setVisible(true);
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+}
